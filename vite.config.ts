5178f598f9444b8d24bb4f30b52ff2634b87514c
huangpu <huangpu@ysstech.com>
2023-03-24 17:54:47 +0800

    删除代理服务

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,4 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://10.10.90.183:8333/',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
 });
